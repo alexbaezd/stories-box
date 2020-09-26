667f4249665350313d118afd366fe7ca8aff8de8
=Alejandro Baez <baezdalex@gmail.com>
2020-09-26 18:24:26 -0500

    🎨 Feat (create schema and function Query)

---
+++ functions/utils/query.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const axios = require("axios");
+
+module.exports = async (query, variables) => {
+  //TODO: create function
+  const result = await axios({
+    url: "https://graphql.fauna.com/graphql",
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.FAUNA_SERVER_SECRET}`,
+    },
+    data: {
+      query,
+      variables,
+    },
+  });
+  return result.data;
+}
