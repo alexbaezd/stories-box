8a63311a2c9b46e099587255a41884553af3aa33
=Alejandro Baez <baezdalex@gmail.com>
2020-10-02 12:32:14 -0500

    🍱  Feat: New Icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,15 +21,15 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#34495E`,
+        theme_color: `#34495E`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/histories-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`
   ],
 }
